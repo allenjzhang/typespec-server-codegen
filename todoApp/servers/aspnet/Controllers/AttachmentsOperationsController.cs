fc53069f9675d7d3867c41556ab8005e055aedbf
Arcturus Zhang <dapzhang@microsoft.com>
2024-11-29 11:55:39 +0800

    implement one of the controller to receive requests

---
+++ todoApp/servers/aspnet/Controllers/AttachmentsOperationsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Todo.Service.Controllers
+{
+    [ApiController]
+    public class AttachmentsOperationsController : AttachmentsOperationsControllerBase
+    {
+        internal override IAttachmentsOperations AttachmentsOperationsImpl => new AttachmentsOperations();
+    }
+}
