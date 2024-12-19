d48b3f1bcea4ea5ed20d5043f0124bc76acba2cc
Weidong Xu <weidxu@microsoft.com>
2024-12-19 10:13:56 +0800

    use Push-Location

--- eng/scripts/java/Generate.ps1
+++ eng/scripts/java/Generate.ps1
@@ -2,20 +2,20 @@ Set-Location (Resolve-Path (Join-Path $PSScriptRoot '..' '..' '..'))
 
 # Generate
 Remove-Item ./petstore/clients/java/src/main -Recurse -Force
-Set-Location ./petstore/spec
+Push-Location ./petstore/spec
 npx --no-install tsp compile . --emit "@typespec/http-client-java"
-Set-Location ../..
+Pop-Location
 
 Remove-Item ./todoApp/clients/java/src/main -Recurse -Force
-Set-Location ./todoApp/spec
+Push-Location ./todoApp/spec
 npx --no-install tsp compile . --emit "@typespec/http-client-java"
-Set-Location ../..
+Pop-Location
 
 # Build (currently require a local install of io.clientcore:core)
-Set-Location ./petstore/clients/java
+Push-Location ./petstore/clients/java
 mvn package -DskipTests
-Set-Location ../../..
+Pop-Location
 
-Set-Location ./todoApp/clients/java
+Push-Location ./todoApp/clients/java
 mvn package -DskipTests
-Set-Location ../../..
+Pop-Location
