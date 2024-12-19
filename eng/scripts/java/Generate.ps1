c72321d9388efc98bd78752a9cce89d8f0a19e7a
Weidong Xu <weidxu@microsoft.com>
2024-12-19 09:43:43 +0800

    update script

--- eng/scripts/java/Generate.ps1
+++ eng/scripts/java/Generate.ps1
@@ -1,5 +1,6 @@
 Set-Location (Resolve-Path (Join-Path $PSScriptRoot '..' '..' '..'))
 
+# Generate
 Remove-Item ./petstore/clients/java/src/main -Recurse -Force
 Set-Location ./petstore/spec
 npx --no-install tsp compile . --emit "@typespec/http-client-java"
@@ -9,3 +10,12 @@ Remove-Item ./todoApp/clients/java/src/main -Recurse -Force
 Set-Location ./todoApp/spec
 npx --no-install tsp compile . --emit "@typespec/http-client-java"
 Set-Location ../..
+
+# Build (currently require a local install of io.clientcore:core)
+Set-Location ./petstore/clients/java
+mvn package -DskipTests
+Set-Location ../../..
+
+Set-Location ./todoApp/clients/java
+mvn package -DskipTests
+Set-Location ../../..
