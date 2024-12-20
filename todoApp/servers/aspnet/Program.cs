9c4f1b2c7d4f5ba4f946984773349a466dd9865f
Arcturus Zhang <dapzhang@microsoft.com>
2024-12-20 11:35:31 +0800

    initial commit

---
+++ todoApp/servers/aspnet/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddControllers(options =>
+//{
+//    options.Filters.Add(new CustomExceptionFilter());
+//}).ConfigureApiBehaviorOptions(options =>
+//{
+//    options.InvalidModelStateResponseFactory = HandleInvalidModelStateResponse;
+//});
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
+
+//static IActionResult HandleInvalidModelStateResponse(ActionContext context)
+//{
+//    var errors = context.ModelState
+//        .Where(e => e.Value != null && e.Value.Errors.Count > 0)
+//        .Select(e => string.Join(',', e.Value.Errors.Select(x => x.ErrorMessage)));
+//    return new JsonResult(new PetStoreError()
+//    {
+//        Code = (int)HttpStatusCode.BadRequest,
+//        Message = string.Join('\n', errors)
+//    })
+//    {
+//        StatusCode = (int)HttpStatusCode.BadRequest
+//    };
+//}
